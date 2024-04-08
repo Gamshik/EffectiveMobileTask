79acdd44a02739f0c60712a0550b29927a90b52a
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-04-08 22:48:40 +0300

    Create Main method

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApplication.OptionExtensions;
+using ConsoleApplication.Options;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ProgramOption options;
+
+            try
+            {
+                options = ProgramOption.ParseOptions(args);
+            }
+            catch (ArgumentNullException argumentNullEx)
+            {
+                Console.WriteLine(argumentNullEx.Message);
+                return;
+            }
+            catch (ArgumentOutOfRangeException argumentOutOfRangeEx)
+            {
+                Console.WriteLine(argumentOutOfRangeEx.Message);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            if (!options.IsValid())
+            {
+                Console.WriteLine("Not all required arguments are initialize.");
+                return;
+            }
+        }
+    }
+}
