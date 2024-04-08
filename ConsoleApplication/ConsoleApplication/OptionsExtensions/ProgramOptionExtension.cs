0eb1ce5b9e230030a7d79d5b9cfceff7f7820d6e
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-04-08 22:48:17 +0300

    Create ProgramOptionExtension
    IsValid - method for validation program option, he is to check existence required arguments

---
+++ ConsoleApplication/ConsoleApplication/OptionsExtensions/ProgramOptionExtension.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApplication.Options;
+
+namespace ConsoleApplication.OptionExtensions
+{
+    public static class ProgramOptionExtension
+    {
+        /// <summary>
+        /// Метод для проверки наличия обязательных параметров в классе
+        /// </summary>
+        /// <param name="option">Класс для валидации</param>
+        /// <returns>Валидный/Невалидный</returns>
+        public static bool IsValid(this ProgramOption option)
+        {
+            return !string.IsNullOrWhiteSpace(option.LogFile)
+                && !string.IsNullOrWhiteSpace(option.OutputFile)
+                && option.TimeStart.HasValue
+                && option.TimeEnd.HasValue;
+        }
+    }
+}
