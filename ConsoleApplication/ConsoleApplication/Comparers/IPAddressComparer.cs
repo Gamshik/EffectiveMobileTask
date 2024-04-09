00beded4577baefad62604b1d4e77a83724adfe5
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-04-09 15:31:22 +0300

    Create IPAddressComparer
    IPAddressComparer - class for comparer two ip adress

---
+++ ConsoleApplication/ConsoleApplication/Comparers/IPAddressComparer.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace ConsoleApplication.Comparers
+{
+    public class IPAddressComparer : IComparer<IPAddress>
+    {
+        public int Compare(IPAddress? firstAddress, IPAddress? secondAddress)
+        {
+            if (firstAddress == null || secondAddress == null)
+                throw new ArgumentNullException($"IPAddress argument can`t be is null.");
+
+            int result;
+
+            var firstAddressBytes = firstAddress.GetAddressBytes();
+            var secondAddressBytes = secondAddress.GetAddressBytes();
+
+            for (int i = 0; i < 4; i++)
+            {
+                result = firstAddressBytes[i].CompareTo(secondAddressBytes[i]);
+
+                if (result != 0)
+                    return result;
+            }
+
+            return 0;
+        }
+    }
+}
