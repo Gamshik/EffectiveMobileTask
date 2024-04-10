13ae7bb04fd3187051bea5e38dbbee571e93ec03
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-04-11 01:13:32 +0300

    Create DataWrapper class
    DataWrapper - is a class which representing parameters in tests methods

---
+++ ConsoleApplication/ConsoleApplication.Tests/DataWrappers/DataWrapper.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApplication.Tests.DataWrappers
+{
+    public class DataWrapper<T, TExp>
+    {
+        public T Value { get; set; }
+        public TExp Expectation { get; set; }
+    }
+}
