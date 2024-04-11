a3d71d9674caf8fc2c4007771c3cee19bfc16d1e
Gamshik <122757204+Gamshik@users.noreply.github.com>
2024-04-12 00:56:36 +0300

    Create DictionaryIpAddressCountTestWrapper
    DictionaryIpAddressCountTestWrapper - is a entity for help present info in test method parameters

---
+++ ConsoleApplication/ConsoleApplication.Tests/DataWrappers/DictionaryIpAddressCountTestWrapper.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApplication.Options;
+using System.Net;
+
+namespace ConsoleApplication.Tests.DataWrappers
+{
+    public class DictionaryIpAddressCountTestWrapper
+    {
+        public string[] Logs { get; set; }
+        public ProgramOption Options { get; set; }
+        public Dictionary<IPAddress, int> Expectation { get; set; }
+    }
+}
